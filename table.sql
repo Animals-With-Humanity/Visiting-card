2ce0eaaeed695af1d71265300d23941323e58ee7
pegasis470 <www.sumantdhere@gmail.com>
2025-03-03 20:29:37 +0530

    basic changes

---
+++ table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    role TEXT NOT NULL,contact TEXT NOT NULL,whatsapp TEXT NOT NULL,about TEXT not null,work_email text not null,personal_email TEXT NOT NULL,linkedin text not null,instagram text not null,image_url text not null,background_image text not null  
+   );
